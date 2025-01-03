511662b8aa94cce2e0289847f4e6c626a0b81337
Aroa <aroa.gonzalez@opendeusto.es>
2025-01-03 16:21:33 +0100

    cambio y mejora del menú

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../menu.css';
 
 function Menu() {
   return (
     <nav className="menu">
       <ul>
-        <li><Link to="/home">Inicio</Link></li>
-        <li><Link to="/pedidos">Gestión de Pedidos</Link></li>
-        <li><Link to="/pedidos-pendientes">Pedidos Pendientes</Link></li>
-        <li><Link to="/lista-pedidos">Pedidos completados</Link></li>
-        <li><Link to="/inventario">Inventario</Link></li>
-        <li><Link to="/consultar-tiendas">Consultar Tiendas</Link></li>
-        <li><Link to="/pedidos-eliminados">Pedidos Eliminados</Link></li>
+        <li>
+          <Link to="/home">Inicio</Link>
+        </li>
+        <li>
+          <Link to="/pedidos">Gestión de Pedidos</Link>
+        </li>
+        <li>
+          <Link to="/pedidos-pendientes">Pedidos Pendientes</Link>
+        </li>
+        <li>
+          <Link to="/lista-pedidos">Pedidos Completados</Link>
+        </li>
+        <li>
+          <Link to="/inventario">Inventario</Link>
+        </li>
+        <li>
+          <Link to="/consultar-tiendas">Consultar Tiendas</Link>
+        </li>
+        <li>
+          <Link to="/pedidos-eliminados">Pedidos Eliminados</Link>
+        </li>
       </ul>
+      <div className="footer-options">
+        <Link to="/profile" className="footer-link">
+          Mi Perfil
+        </Link>
+        <button onClick={() => alert('Sesión cerrada')} className="footer-button">
+          Cerrar Sesión
+        </button>
+      </div>
     </nav>
   );
 }
