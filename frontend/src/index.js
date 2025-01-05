98297fceed8ce94bf91d84566670c480b936064a
Aroa <aroa.gonzalez@opendeusto.es>
2025-01-05 12:37:30 +0100

    css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Cambia 'react-dom' por 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 
-// Utiliza createRoot para React 18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
