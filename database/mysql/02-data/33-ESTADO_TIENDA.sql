13cf43ab9fb16dbd35383c8aecadfe936f8bff1c
Aroa <aroa.gonzalez@opendeusto.es>
2025-03-30 11:35:15 +0200

    bbdd

---
+++ database/mysql/02-data/33-ESTADO_TIENDA.sql
@@ -0,0 +1,17 @@
+USE MAESTROS;
+
+INSERT INTO MAESTROS.ESTADO_TIENDA (ID_ESTADO_TIENDA,DESCRIPCION) VALUES
+	 (1,'Provisional'),
+	 (2,'Inminente Apertura'),
+	 (3,'Abierta'),
+	 (4,'En Reforma'),
+	 (5,'Baja Próxima'),
+	 (6,'Cerrada Definitivamente'),
+	 (7,'Obra Menor'),
+	 (8,'Reubicación'),
+	 (9,'Inminente Reapertura'),
+	 (10,'Próxima Reforma');
+INSERT INTO MAESTROS.ESTADO_TIENDA (ID_ESTADO_TIENDA,DESCRIPCION) VALUES
+	 (11,'Con Mov.Prev.Apertura'),
+	 (12,'Deshabilitada'),
+	 (13,'Cerrada Recientemente');
---
+++ database/mysql/02-data/34-ESTADO_TIENDA_IDIOMA.sql
@@ -0,0 +1,31 @@
+USE MAESTROS;
+
+INSERT INTO MAESTROS.ESTADO_TIENDA_IDIOMA (ID_ESTADO_TIENDA,ID_IDIOMA,DESCRIPCION) VALUES
+	 (3,1,'Open'),
+	 (6,1,'Definitively Closed'),
+	 (4,1,'Under Reform'),
+	 (1,1,'Provisional'),
+	 (3,3,'Abierta'),
+	 (6,3,'Cerrada Definitivamente'),
+	 (4,3,'En Reforma'),
+	 (1,3,'Provisional'),
+	 (2,3,'Inminente Apertura'),
+	 (2,1,'Imminent Opening');
+INSERT INTO MAESTROS.ESTADO_TIENDA_IDIOMA (ID_ESTADO_TIENDA,ID_IDIOMA,DESCRIPCION) VALUES
+	 (5,3,'Baja Próxima'),
+	 (5,1,'Forthcoming Cancellation'),
+	 (7,3,'Obra Menor'),
+	 (7,1,'Minor Work'),
+	 (8,3,'Reubicación'),
+	 (8,1,'Relocation'),
+	 (9,3,'Inminente Reapertura'),
+	 (9,1,'Imminent Reopening'),
+	 (10,3,'Próxima Reforma'),
+	 (10,1,'Forthcoming Reform');
+INSERT INTO MAESTROS.ESTADO_TIENDA_IDIOMA (ID_ESTADO_TIENDA,ID_IDIOMA,DESCRIPCION) VALUES
+	 (11,3,'Con Mov.Prev.Apertura'),
+	 (11,1,'With Prev.Opening Mov.'),
+	 (12,3,'Deshabilitada'),
+	 (12,1,'Disabled'),
+	 (13,3,'Cerrada Recientemente'),
+	 (13,1,'Recently Closed');
